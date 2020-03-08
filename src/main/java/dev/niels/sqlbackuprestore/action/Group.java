d7c8e021f6d4c32746c5066e09eaf7d2f8c246f6
Niels <n.van.de.weem@smile.nl>
2020-03-08 15:09:38 +0100

    Hide actions for non-sqlserver databases

---
+++ src/main/java/dev/niels/sqlbackuprestore/action/Group.java
@@ -0,0 +1,13 @@
+package dev.niels.sqlbackuprestore.action;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import dev.niels.sqlbackuprestore.query.QueryHelper;
+import org.jetbrains.annotations.NotNull;
+
+public class Group extends DefaultActionGroup {
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        e.getPresentation().setVisible(QueryHelper.isMssql(e));
+    }
+}
